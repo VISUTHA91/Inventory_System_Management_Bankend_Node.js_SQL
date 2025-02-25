b684141f2527fc565490bdb8b0e3239f0e32b4ac
Your Name <ishudarju46@gmail.com>
2025-02-25 10:28:50 +0530

    Add authentication middleware to expense routes for enhanced security

--- routes/Expense_routes.js
+++ routes/Expense_routes.js
@@ -4,31 +4,31 @@ const router = express.Router();
 const { authMiddleware, adminOnly, limiter, adminOrStaff } = require("../middleware/auth_middleware");
 
 // Create a new expense
-router.post('/expensesinsert', adminOnly,expenseController.createExpense);
+router.post('/expensesinsert',authMiddleware, adminOnly,expenseController.createExpense);
 
 // Get all expenses
-router.get('/expensesall', adminOnly,expenseController.getAllExpenses);
+router.get('/expensesall', authMiddleware,adminOnly,expenseController.getAllExpenses);
 
-router.get('/pagination_expence', adminOnly,expenseController.getAllExpensespage);
+router.get('/pagination_expence', authMiddleware,adminOnly,expenseController.getAllExpensespage);
 
 // Get a single expense by ID
-router.get('/expenses/:id', adminOnly,expenseController.getExpenseById);
+router.get('/expenses/:id', authMiddleware,adminOnly,expenseController.getExpenseById);
 
 // Update an expense
-router.put('/expensesedit/:id',adminOnly, expenseController.updateExpense);
+router.put('/expensesedit/:id',authMiddleware,adminOnly, expenseController.updateExpense);
 
 // Delete an expense
-router.delete('/expensesdel/:id',adminOnly, expenseController.deleteExpense);
+router.delete('/expensesdel/:id',authMiddleware,adminOnly, expenseController.deleteExpense);
 
 //expense details
 
-router.get('/daily/expense', adminOnly,expenseController.getDailyExpenseReport);
+router.get('/daily/expense',authMiddleware, adminOnly,expenseController.getDailyExpenseReport);
 
 
-router.get('/monthly/expense', adminOnly,expenseController.getMonthlyExpenseReport); 
+router.get('/monthly/expense', authMiddleware,adminOnly,expenseController.getMonthlyExpenseReport); 
 
  // New route for monthly expense
-router.get('/yearly/expense', adminOnly,expenseController.getYearlyExpenseReport); 
+router.get('/yearly/expense', authMiddleware,adminOnly,expenseController.getYearlyExpenseReport); 
 
 
 module.exports = router;
